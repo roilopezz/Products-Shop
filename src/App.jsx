4ce950ee12f5ad7c451aa69874fc80bc9f3828cf
roilopezz <roilopez946@gmail.com>
2022-04-04 20:47:28 +0300

    update-3 04/04/2022

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
-// import NavBar from "./components/pages/navbar/navbar";
 import Dashboard from "./components/dashboard";
 import Footer from "./components/footer/footer";
 import ShopContext from "./services/shopContext";
 import UserContext from "./services/userContext";
-
-// import Footer from "./components/pages/footer/footer";
 
 function App() {
   return (
